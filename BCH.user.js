ad9b2cb1fe8e3afe79f1b76cd74f101b29f923b1
Wozzey <robernesje@yahoo.no>
2022-04-04 04:48:45 +0200

    test

--- BCH.user.js
+++ BCH.user.js
@@ -38,7 +38,7 @@ var bcModSdk=function(){"use strict";const o="1.0.2";function e(o){alert("Mod ER
 var BCH_VERSION = "1.0";
 
 // FUCK JAVASCRIPT
-import util from "./src/utils.js";
+import * as util from "./src/utils.js";
 
 async function BondageClubHelper() {
     "use strict";
