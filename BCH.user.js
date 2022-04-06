f4a4c5504fc2dd9b3787ec33d284a654087b042c
NarikoNep <nepushiro@gmail.com>
2022-04-06 07:16:42 -0500

    Fix typo

--- BCH.user.js
+++ BCH.user.js
@@ -442,11 +442,11 @@ async function BondageClubHelper() {
 					} else if (Emoticon && Emoticon.Property && Emoticon.Property.Expression == "Gaming" && AntiBindMember[i] == AntiBind) {
 						Player.ItemPermission = 5;
 						ServerAccountUpdate.QueueData({ItemPermission: Player.ItemPermission}, true);
-					} else if (Player.ItemPermission == 1 && Emoticon && Emoticon.Property && Emoticon.Property.Expression == undefined && AntiBindMember[i] == AntiBind) {
-						return;
 					} else if (Player.ItemPermission == 3 || Player.ItemPermission == 5 && Emoticon && Emoticon.Property && Emoticon.Property.Expression != "Sleep" || Emoticon.Property.Expression != "Gaming" && AntiBindMember[i] == AntiBind) {
 						Player.ItemPermission = 1;
 						ServerAccountUpdate.QueueData({ItemPermission: Player.ItemPermission}, true);
+					} else if (Player.ItemPermission == 1 && Emoticon && Emoticon.Property && Emoticon.Property.Expression == undefined && AntiBindMember[i] == AntiBind) {
+						return;
 					}
 				}
 			}
