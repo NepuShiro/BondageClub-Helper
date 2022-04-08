c476475548827c7cd39c1afea0feb4438be81f73
NarikoNep <nepushiro@gmail.com>
2022-04-08 02:16:19 -0500

    work

--- BCH.user.js
+++ BCH.user.js
@@ -443,7 +443,7 @@ async function BondageClubHelper() {
 					Player.ItemPermission = 5;
 					ServerAccountUpdate.QueueData({ItemPermission: Player.ItemPermission}, true);
 					bchLog("ItemsPerms have been updated to 5");
-				} else if (Player.ItemPermission > 1 && Emoticon && Emoticon.Property == null && Emoticon.Property.Expression == undefined && AntiBindMember[i] == AntiBind) {
+				} else if (Player.ItemPermission > 1 && Emoticon && Emoticon.Property == null || Emoticon.Property == undefined && AntiBindMember[i] == AntiBind) {
 					Player.ItemPermission = 1;
 					ServerAccountUpdate.QueueData({ItemPermission: Player.ItemPermission}, true);
 				} else if (Player.ItemPermission > 1 && Emoticon.Property.Expression != "Gaming" || Emoticon.Property.Expression != "Sleep" && AntiBindMember[i] == AntiBind) {
