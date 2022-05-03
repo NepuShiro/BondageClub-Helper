aee9b69075fcc957aae3ec6078a32be986bad86b
NarikoNep <nepushiro@gmail.com>
2022-05-03 14:52:51 -0500

    Change description

--- BCH.user.js
+++ BCH.user.js
@@ -102,7 +102,7 @@ async function BondageClubHelper() {
 			category: "General",
 		},
 		EmoticonBlock: {
-			label: "Block actions emoticons",
+			label: "Block actions based on emoticon",
 			value: false,
 			sideEffects: (newValue) => {
 					if (newValue) {
