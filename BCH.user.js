4aa4bf89fd5d6e8fd33f72603a89eb7f1febefcc
NarikoNep <nepushiro@gmail.com>
2022-05-03 14:57:02 -0500

    Change Color and position of overlay

--- BCH.user.js
+++ BCH.user.js
@@ -775,8 +775,8 @@ async function BondageClubHelper() {
 				const ret = next(args);
 				const [C, CharX, CharY, Zoom] = args;
 				if (isCharacter(C) && typeof CharX === "number" && typeof CharY === "number" && typeof Zoom === "number" && C.BCH && ChatRoomHideIconState === 0) {
-					DrawImageResize(ICONS.USER, CharX + 225 * Zoom, CharY, 50 * Zoom, 50 * Zoom);
-					DrawTextFit(C.BCH, CharX + 250 * Zoom, CharY + 40 * Zoom, 50 * Zoom, DEVS.includes(C.MemberNumber) ? "#b33cfa" : "White", "Black" );
+					DrawImageResize(ICONS.USER, CharX + 220 * Zoom, CharY, 55 * Zoom, 50 * Zoom);
+					DrawTextFit(C.BCH, CharX + 245 * Zoom, CharY + 40 * Zoom, 50 * Zoom, DEVS.includes(C.MemberNumber) ? "#60216c" : "White", "Black" );
 				}
 				return ret;
 			}
