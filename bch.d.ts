c65bbd1ee9e6dde6075609c3e54b79e2538ecd1f
NarikoNep <nepushiro@gmail.com>
2022-05-04 13:00:11 -0500

    Sort ts

--- bch.d.ts
+++ bch.d.ts
@@ -3,112 +3,30 @@
 export { };
 
 declare global {
-  var BCH_VERSION: string;
+  // functions
   var bchSendAction: (text: string) => void;
   var bchSettingValue: (key: string) => boolean | number | string;
-  var Player: Character;
-  var ServerAccountUpdate: AccountUpdater;
   var ChatRoomCurrentTime: () => string;
-  var LZString: LZStringType;
   var ElementIsScrolledToEnd: (element: string) => boolean;
   var ElementScrollToEnd: (element: string) => void;
-  var ServerBeep: ServerBeep;
   var ServerSend: (event: string, data: unknown) => void;
-  var GameVersion: string;
-  var StruggleProgress: number;
   var ChatRoomCanBeLeashedBy: (id: number, C: Character) => boolean;
   var ChatRoomPlayerIsAdmin: () => boolean;
   var ActivityOrgasmStart: (C: Character) => void;
-  var DialogLentLockpicks: boolean;
   var ChatRoomSetLastChatRoom: (String) => String;
   var CharacterDeleteAllOnline: () => void;
   var ChatSearchExit: () => void;
   var CharacterReleaseTotal: (C: Character) => void;
-  var CharacterAppearanceReturnRoom: string;
-  var CharacterAppearanceReturnModule: string;
   var ChatRoomStatusUpdate: (Status: string) => void;
   var CharacterAppearanceLoadCharacter: (C: Character) => void;
   var ChatRoomCanLeave: () => boolean;
   var MainHallWalk: (room: string) => void;
-  var CurrentScreen: string;
   var ChatRoomCharacterUpdate: (C: Character) => void;
-  var ChatRoomCharacter: Character[];
-  var Character: Character[];
-  var PreferenceSubscreenList: string[];
-  var PreferenceSubscreen: string;
-  var PreferenceMessage: string;
-  var DrawText: (
-    text: string,
-    x: number,
-    y: number,
-    color: string,
-    backColor?: string
-  ) => void;
-  var DrawTextFit: (
-    text: string,
-    x: number,
-    y: number,
-    w: number,
-    color: string,
-    backColor?: string
-  ) => void;
-  var DrawButton: (
-    x: number,
-    y: number,
-    w: number,
-    h: number,
-    label: string,
-    color: string,
-    image?: string,
-    hoveringText?: string,
-    disabled?: boolean
-  ) => void;
-  var DrawCheckbox: (
-    x: number,
-    y: number,
-    w: number,
-    h: number,
-    text: string,
-    isChecked: boolean,
-    disabled?: boolean,
-    textColor?: string,
-    checkImage?: string
-  ) => void;
-  var DrawImageResize: (
-    image: string,
-    x: number,
-    y: number,
-    w: number,
-    h: number
-  ) => boolean;
-  var MouseIn: (x: number, y: number, w: number, h: number) => boolean;
-  var ServerIsConnected: boolean;
-  var ServerSocket: ServerSocket;
-  var Commands: Command[];
-  var CharacterRefresh: (
-    C: Character,
-    push?: boolean,
-    refreshDialog?: boolean
-  ) => void;
-  var CurrentCharacter: Character;
-  var ChatRoomHideIconState: number;
-  var ChatRoomData: {
-    Admin: any;
-    Game: string;
-    Locked: any; Background: string; Name: string
-  };
-  var WardrobeFastLoad: (
-    C: Character,
-    position: number,
-    update?: boolean
-  ) => void;
-  var BCX_Loaded: boolean;
   var PreferenceSubscreenBCHSettingsLoad: () => void;
   var PreferenceSubscreenBCHSettingsExit: () => void;
   var PreferenceSubscreenBCHSettingsRun: () => void;
   var PreferenceSubscreenBCHSettingsClick: () => void;
   var CommonSetScreen: (category: string, screen: string) => void;
-  var ChatRoomLeashPlayer: number;
   var ChatRoomClearAllElements: () => void;
   var ValidationCanRemoveItem: () => boolean;
   var InventoryGetItemProperty: (item: Array, assettype: string, a: boolean) => boolean;
@@ -116,6 +34,41 @@ declare global {
   var InventoryOwnerOnlyItem: (previousItem: string) => boolean;
   var InventoryLoverOnlyItem: (previousItem: string) => boolean;
   var InventoryGetLock: (previousitem: Array) => Array;
+  var DrawText: (text: string, x: number, y: number, color: string, backColor?: string) => void;
+  var DrawTextFit: (text: string, x: number, y: number, w: number, color: string, backColor?: string) => void;
+  var DrawButton: (x: number, y: number, w: number, h: number, label: string, color: string, image?: string, hoveringText?: string, disabled?: boolean) => void;
+  var DrawCheckbox: (x: number, y: number, w: number, h: number, text: string, isChecked: boolean, disabled?: boolean, textColor?: string, checkImage?: string) => void;
+  var DrawImageResize: (image: string, x: number, y: number, w: number, h: number) => boolean;
+  var MouseIn: (x: number, y: number, w: number, h: number) => boolean;
+  var CharacterRefresh: (C: Character, push?: boolean, refreshDialog?: boolean ) => void;
+  var WardrobeFastLoad: (C: Character, position: number, update?: boolean) => void;
+
+  // variables
+  var Character: Character[];
+  var BCH_VERSION: string;
+  var Player: Character;
+  var ServerAccountUpdate: AccountUpdater;
+  var LZString: LZStringType;
+  var ServerBeep: ServerBeep;
+  var GameVersion: string;
+  var StruggleProgress: number;
+  var DialogLentLockpicks: boolean;
+  var CharacterAppearanceReturnRoom: string;
+  var CharacterAppearanceReturnModule: string;
+  var CurrentScreen: string;
+  var ChatRoomLeashPlayer: number;
+  var ChatRoomCharacter: Character[];
+  var PreferenceSubscreenList: string[];
+  var PreferenceSubscreen: string;
+  var PreferenceMessage: string;
+  var ServerIsConnected: boolean;
+  var ServerSocket: ServerSocket;
+  var Commands: Command[];
+  var CurrentCharacter: Character;
+  var ChatRoomHideIconState: number;
+  var ChatRoomData: {Admin: any; Game: string; Locked: any; Background: string; Name: string};
+  var BCX_Loaded: boolean;
+
 }
 declare global {
   interface Window {
@@ -127,15 +80,15 @@ declare global {
     "Experimental";
   type Character = {
     CanWalk: () => boolean;
-    ItemPermission: number;
+    CanChangeOwnClothes?: () => boolean;
+    IsRestrained: () => boolean;
     CanChangeClothesOn: (C: Character) => boolean;
     IsSlow: () => boolean;
+    ItemPermission: number;
     Effect: string;
     ID: number;
     Pose: string;
-    RestrictionSettings: {
-      SlowImmunity: boolean;
-    };
+    RestrictionSettings: {SlowImmunity: boolean;};
     OnlineSettings: OnlineSettings;
     MemberNumber: number;
     Name: string;
@@ -143,10 +96,7 @@ declare global {
     Appearance: Item[];
     BCH: string;
     BCHOriginalName?: string;
-    CanChangeOwnClothes?: () => boolean;
-    IsRestrained: () => boolean;
   };
-
   type BCHChatMessage = ChatMessageBase & {
     Dictionary: { message: BCHMessage };
   };
